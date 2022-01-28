5b5ff7928a874045703635f7d3b2bcf602e65180
Nathan Samson <nathan@nathansamson.be>
2022-01-28 22:15:40 +0100

    Initial version

---
+++ src/channel_builder.rs
@@ -0,0 +1,72 @@
+use std::mem;
+use std::os::raw::{c_int, c_uint};
+
+use super::bindings;
+use super::strip_type::StripType;
+
+pub struct ChannelBuilder(pub bindings::ws2811_channel_t);
+
+impl ChannelBuilder {
+    /// Creates a new ChannelBuilder
+    pub fn new() -> Self {
+        unsafe { ChannelBuilder(mem::zeroed()) }
+    }
+    /// Sets the GPIO pin used by this channel
+    pub fn pin(&mut self, value: i32) -> &mut Self {
+        self.0.gpionum = value as c_int;
+        self
+    }
+    /// Sets the number of LEDs attached to this channel
+    pub fn count(&mut self, value: i32) -> &mut Self {
+        self.0.count = value as c_int;
+        self
+    }
+    /// Sets the strip type of this channel
+    pub fn strip_type(&mut self, value: StripType) -> &mut Self {
+        let tmp: c_uint = value.into();
+        self.0.strip_type = tmp as i32;
+        self
+    }
+    /// Sets the invert flag on the channel
+    pub fn invert(&mut self, value: bool) -> &mut Self {
+        if value {
+            self.0.invert = 1 as c_int;
+        } else {
+            self.0.invert = 0 as c_int;
+        }
+        self
+    }
+    /// Sets the brightness of this channel
+    pub fn brightness(&mut self, value: u8) -> &mut Self {
+        self.0.brightness = value;
+        self
+    }
+    /// Sets the white shift of this channel
+    pub fn wshift(&mut self, value: u8) -> &mut Self {
+        self.0.wshift = value;
+        self
+    }
+    /// Sets the red shift of this channel
+    pub fn rshift(&mut self, value: u8) -> &mut Self {
+        self.0.rshift = value;
+        self
+    }
+    /// Sets the green shift of this channel
+    pub fn gshift(&mut self, value: u8) -> &mut Self {
+        self.0.gshift = value;
+        self
+    }
+    /// Sets the blue shift of this channel
+    pub fn bshift(&mut self, value: u8) -> &mut Self {
+        self.0.bshift = value;
+        self
+    }
+    /// Return the built channel.
+    pub fn build(&mut self) -> bindings::ws2811_channel_t {
+        // all of the pointers will be initialized as a part of
+        // ws2811_init() in the controller builder, so clone
+        // here so that this builder can have .build() called
+        // multiple times.
+        self.0.clone()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+use std::mem;
+use std::slice::from_raw_parts_mut;
+
+mod bindings;
+mod channel_builder;
+mod error;
+mod strip_type;
+
+use channel_builder::ChannelBuilder;
+use error::WS2811Error;
+use strip_type::StripType;
+
+use smart_leds_trait::{SmartLedsWrite, RGB8};
+
+pub struct Ws2812Rpi {
+    c_struct: bindings::ws2811_t,
+}
+
+impl Ws2812Rpi
+{
+    /// Use ws2812 devices via rpi-ws281x library
+    pub fn new() -> Result<Self, WS2811Error> {
+        unsafe {
+            let mut ret = Self { c_struct: mem::zeroed() };
+
+            ret.c_struct.freq = 800_000;
+            ret.c_struct.dmanum = 10;
+            ret.c_struct.channel[0] = ChannelBuilder::new()
+                                     .pin(18)
+                                     .count(10)
+                                     .strip_type(StripType::Ws2811Rgb)
+                                     .brightness(255)
+                                     .build();
+            let res: Result<(), WS2811Error> = bindings::ws2811_init(&mut ret.c_struct).into();
+
+            match res {
+                Ok(_) => {}
+                Err(e) => return Err(e),
+            }
+            return Ok(ret);
+        }
+
+    }
+
+}
+
+impl Drop for Ws2812Rpi {
+    fn drop(&mut self) {
+        /*
+         * Unsafe used here because we need to call an externed
+         * function during the drop process.  Unfortunately,
+         * I don't have a better way of dealing with this.
+         */
+        unsafe {
+            bindings::ws2811_fini(&mut self.c_struct);
+        }
+    }
+}
+
+impl SmartLedsWrite for Ws2812Rpi
+{
+    type Error = error::WS2811Error;
+    type Color = RGB8;
+    // Write all the items of an iterator to a ws2812 strip
+    fn write<T, I>(&mut self, iterator: T) -> Result<(), Self::Error>
+    where
+        T: Iterator<Item = I>,
+        I: Into<Self::Color>,
+    {
+        let mut i = 0;
+
+        let mut colors = unsafe {
+            from_raw_parts_mut(
+                self.c_struct.channel[0].leds as *mut [u8; 4],
+                self.c_struct.channel[0].count as usize,
+            )
+        };
+
+        for c in iterator {
+            let c_rgb: Self::Color = c.into();
+            colors[i] = [c_rgb.g, c_rgb.r, c_rgb.b, 0];
+            i = i + 1;
+        }
+
+        unsafe {
+            return bindings::ws2811_render(&mut self.c_struct).into();
+        }
+
+        Ok(())
+    }
+}
